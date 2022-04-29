27b90a18e2cec00801fcaf490c2bc15fad345cf6
malayvasa <malayvasa@gmail.com>
2022-04-29 08:44:17 +0530

    Added p5JS and HighlightSketch

--- src/components/highlight-modal.js
+++ src/components/highlight-modal.js
@@ -1,11 +1,19 @@
 import { useState } from 'react';
+import { ReactP5Wrapper } from "react-p5-wrapper";
+import HighlightSketch from '../HighlightSketch';
 
 function HighlightModal({ setModalVisible, selectedText }) {
   return (
-    <div className="flex justify-center relative my-auto w-[800px] h-[500px] rounded-lg shadow-xl bg-slate-100 ">
+    <div className="flex justify-center relative my-auto w-[600px] rounded-lg shadow-xl bg-gray-100 ">
      
-     <div className='flex flex-row gap-x-8 my-auto p-32 font-redaction text-2xl'>
-      {selectedText}
+     <div className='flex flex-col gap-y-8 p-8 my-auto'>
+         <div className='rounded-lg shadow-lg'>
+         <ReactP5Wrapper sketch={HighlightSketch} selectedText={selectedText} />
+         </div>
+      
+      <div className='w-[500px] font-redaction text-sm text-center'>
+        {selectedText}
+      </div>
      </div>
      
       <div
